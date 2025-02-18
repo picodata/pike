eb761497711370f8df2ac5ddf664b19002be5d42
dodokek <alex.morozov.dd@mail.ru>
2025-02-18 23:59:31 +0300

    fix: adjust pack command for running in workspace

--- src/commands/plugin/pack.rs
+++ src/commands/plugin/pack.rs
@@ -4,10 +4,10 @@ use flate2::Compression;
 use lib::{cargo_build, BuildType};
 use serde::Deserialize;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 use std::{env, fs};
 use tar::Builder;
+use toml::Value;
 
 use crate::commands::lib;
 
@@ -30,11 +30,6 @@ const LIB_EXT: &str = "dylib";
 
 pub fn cmd(pack_debug: bool, target_dir: &PathBuf) -> Result<()> {
     let root_dir = env::current_dir()?;
-    let plugin_name = &root_dir
-        .file_name()
-        .context("extracting project name")?
-        .to_str()
-        .context("parsing filename to string")?;
 
     let build_dir = if pack_debug {
         cargo_build(BuildType::Debug, target_dir).context("building release version of plugin")?;
@@ -44,24 +39,35 @@ pub fn cmd(pack_debug: bool, target_dir: &PathBuf) -> Result<()> {
         Path::new(&root_dir).join(target_dir).join("release")
     };
 
-    let mut manifest_dir = root_dir.clone();
-    // Workaround for case, when plugins is a subcrate of workspace
-    {
-        let cargo_toml_file: File = File::open(root_dir.join("Cargo.toml")).unwrap();
-        let toml_reader = BufReader::new(cargo_toml_file);
+    let plugin_dir = root_dir.clone();
 
-        for line in toml_reader.lines() {
-            let line = line?;
-            if line.contains("workspace") {
-                manifest_dir = root_dir.join(plugin_name);
-                break;
+    let cargo_toml_path = Path::new("Cargo.toml");
+    let cargo_toml_content =
+        fs::read_to_string(cargo_toml_path).expect("Failed to read Cargo.toml");
+
+    let parsed_toml: Value = cargo_toml_content
+        .parse()
+        .context("Failed to parse Cargo.toml")?;
+
+    if let Some(workspace) = parsed_toml.get("workspace") {
+        if let Some(members) = workspace.get("members") {
+            if let Some(members_array) = members.as_array() {
+                for member in members_array {
+                    if let Some(member_str) = member.as_str() {
+                        create_plugin_archive(&build_dir, &root_dir.join(member_str))?;
+                    }
+                }
             }
         }
+        return Ok(());
     }
 
+    create_plugin_archive(&build_dir, &plugin_dir)
+}
+
+fn create_plugin_archive(build_dir: &Path, plugin_dir: &Path) -> Result<()> {
     let cargo_manifest: CargoManifest = toml::from_str(
-        &fs::read_to_string(manifest_dir.join("Cargo.toml"))
-            .context("failed to read Cargo.toml")?,
+        &fs::read_to_string(plugin_dir.join("Cargo.toml")).context("failed to read Cargo.toml")?,
     )
     .context("failed to parse Cargo.toml")?;
 
@@ -78,27 +84,33 @@ pub fn cmd(pack_debug: bool, target_dir: &PathBuf) -> Result<()> {
     let mut encoder = GzEncoder::new(compressed_file, Compression::best());
 
     let lib_name = format!("lib{normalized_package_name}.{LIB_EXT}");
-    let mut lib_file =
-        File::open(build_dir.join(&lib_name)).context(format!("failed to open {lib_name}"))?;
 
-    let mut manifest_file =
-        File::open(build_dir.join("manifest.yaml")).context("failed to open file manifest.yaml")?;
     {
         let mut tarball = Builder::new(&mut encoder);
 
-        tarball
-            .append_file(lib_name, &mut lib_file)
-            .context(format!(
-                "failed to append lib{normalized_package_name}.{LIB_EXT}"
-            ))?;
+        if build_dir.join(&lib_name).exists() {
+            let mut lib_file = File::open(build_dir.join(&lib_name))
+                .context(format!("failed to open {lib_name}"))?;
+            tarball
+                .append_file(lib_name, &mut lib_file)
+                .context(format!(
+                    "failed to append lib{normalized_package_name}.{LIB_EXT}"
+                ))?;
+        }
 
-        tarball
-            .append_file("manifest.yaml", &mut manifest_file)
-            .context("failed to add manifest.yaml to archive")?;
+        if build_dir.join("manifest.yaml").exists() {
+            let mut manifest_file = File::open(build_dir.join("manifest.yaml"))
+                .context("failed to open file manifest.yaml")?;
+            tarball
+                .append_file("manifest.yaml", &mut manifest_file)
+                .context("failed to add manifest.yaml to archive")?;
+        }
 
-        tarball
-            .append_dir_all("migrations", build_dir.join("migrations"))
-            .context("failed to append \"migrations\" to archive")?;
+        if build_dir.join("migrations").exists() {
+            tarball
+                .append_dir_all("migrations", build_dir.join("migrations"))
+                .context("failed to append \"migrations\" to archive")?;
+        }
     }
 
     encoder.finish()?;
